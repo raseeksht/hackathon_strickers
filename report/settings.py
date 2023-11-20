3235c5636308ed0f56870f1e9e1552094da7d4db
Raseek Shrestha <39650487+raseeksht@users.noreply.github.com>
2023-11-20 10:26:45 +0000

    deployment

--- report/settings.py
+++ report/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'report.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.getenv('POSTGRES_URL'),
+        'NAME': os.getenv('PGNAME'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
